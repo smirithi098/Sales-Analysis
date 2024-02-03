37616b620fccc14ebfe43c6c9ad5c5cfd1e76065
Smirithi <smirithi898@gmail.com>
2024-02-03 13:36:19 +0000

    Add data files and schema

---
+++ sales/salesdb_schema.sql
@@ -0,0 +1,15 @@
+﻿CREATE DATABASE sales_db;
+
+USE sales_db;
+
+ALTER TABLE [inventory] WITH CHECK ADD CONSTRAINT [FK_inventory_products] FOREIGN KEY([Product_ID])
+REFERENCES [products] ([Product_ID])
+
+ALTER TABLE [inventory] WITH CHECK ADD CONSTRAINT [FK_inventory_stores] FOREIGN KEY([Store_ID])
+REFERENCES [stores] ([Store_ID])
+
+ALTER TABLE [sales] WITH CHECK ADD CONSTRAINT [FK_sales_stores] FOREIGN KEY([Store_ID])
+REFERENCES [stores] ([Store_ID])
+
+ALTER TABLE [sales] WITH CHECK ADD CONSTRAINT [FK_sales_products] FOREIGN KEY([Product_ID])
+REFERENCES [products] ([Product_ID])
